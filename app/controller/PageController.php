30d470912108b3c4153ffecf57eb4ab5c1771380
Davidfhk <davidign1688@hotmail.com>
2017-05-20 20:37:23 +0200

    el PageController, si no se llama a ningun controlador, se activara este por defecto

---
+++ app/controller/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace David\Crud\Controller;
+
+class PageController
+{
+	public function index(){
+		echo "Bienvenido";
+	}
+
+	public function about($id){
+		echo "Web desarrollada por... David";
+	}
+}
