d741c581f615adbf74a816c6f1770a5bb3b8608b
Davidfhk <davidign1688@hotmail.com>
2017-05-22 19:11:40 +0200

    Creamos la template page, la cual llamaremos desde nuestro PageController

---
+++ app/templates/page/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Bienvenido</h1>
+	<a href="<?= 'user/'?>">Click aqui para entrar al CRUD</a>
+</body>
+</html>
